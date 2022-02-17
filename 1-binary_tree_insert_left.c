2ca0a72d576bd1da24748342969b1dfcf0e35831
Dennis Kiplang'at <denniskiplangat.dk@gmail.com>
2022-02-17 16:32:19 +0300

    Task 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Create a node
+ * @parent: node parent
+ * @value: value of the node (weight)
+ * Return: the new node
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new_node = NULL;
+
+    if(parent == NULL)
+        return(NULL);
+    new_node = malloc(sizeof(binary_tree_t));
+    // new_node = calloc(1, sizeof(binary_tree_t));
+
+    if (new_node == NULL)
+        return(NULL);
+    new_node->parent = parent;
+    new_node->n = value;
+    if(parent->left != NULL)
+    {
+        new_node->left = parent->left;
+        new_node->left->parent = new_node;
+
+    }
+    parent->left = new_node;
+    return(new_node);
+}
