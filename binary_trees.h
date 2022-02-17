9f7f3db40536dee509a7be01e8bcfbe1bd7928ef
Dennis Kiplang'at <denniskiplangat.dk@gmail.com>
2022-02-17 16:14:28 +0300

    Task 0

--- binary_trees.h
+++ binary_trees.h
@@ -2,11 +2,9 @@
 #define BINARY_TREES_H
 
 #include <stddef.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-/* Data structres */
+
 
 /**
  * struct binary_tree_s - Tree node
@@ -16,13 +14,38 @@
  * @right: Pointer to right child node
  */
 
-struct binary_trees_s {
+struct binary_tree_s
+{
 	int n;
-	struct binary_trees_s *parent;
-	struct binary_trees_s *left;
-	struct binary_trees_s *right;
-}
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
 
-typedef struct binary_tree_s bst_t
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+
+
+/* Printing helper function */
+void binary_tree_print(const binary_tree_t *);
+
+
+/* Task 0 */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+
+/* Task 1 */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
+/* Task 2 */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+/* Task 3 */
+void binary_tree_delete(binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
+
+
+
+
